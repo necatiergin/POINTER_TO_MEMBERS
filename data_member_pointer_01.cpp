fef7993a41ce07b3c55cad3e97a14bf26203fb17
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-12-07 09:18:41 +0300

    Update data_member_pointer_01.cpp

--- data_member_pointer_01.cpp
+++ data_member_pointer_01.cpp
@@ -6,15 +6,17 @@ struct A {
 	//...
 };
 
+
 int main()
 {
 	A ax;
 
-	std::cout << "ax.mx = " << ax.mx << "\n";
+	std::cout << "ax.mx = " << ax.mx << ;
 
 	int A::* ptr = &A::mx;
 	ax.*ptr = 100;
-	std::cout << "ax.mx = " << ax.mx << "\n";
+	std::cout << "ax.mx = " << ax.mx << ;
+
 	std::invoke(ptr, ax) = 200;
-	std::cout << "ax.mx = " << ax.mx << "\n";
+	std::cout << "ax.mx = " << ax.mx << ;
 }
